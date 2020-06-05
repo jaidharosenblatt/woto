06b056ad737cd3ecd3d4b1ceb823f78da15fb12d
Matthew Sclar <sclarmatthew@gmail.com>
2020-06-05 12:57:19 -0400

    SignUpForm updated styling and text

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, InputNumber, Button,CheckBox } from 'antd';
+import { Form, Input, InputNumber, Button } from 'antd';
 import './SignUpForm.css'
 
 
@@ -8,6 +8,7 @@ const validateMessages = {
   types: {
     email: '${label} is not validate email!',
     number: '${label} is not a validate number!',
+    password: '${label} is not a validate password!'
   },
   number: {
     range: '${label} must be between ${min} and ${max}',
@@ -22,45 +23,37 @@ const SignUpForm = () => {
   return (
 
     <div className ="container">
-    <Form name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-      <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]}>
+    <p className = "header"> Be among the first to <b>revolutionize</b> office hours </p>
+    <Form name="nest-messages" onFinish={onFinish} validateMessages={validateMessages} labelCol = {{span: 24, offset: 0}}>
+      <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]} >
         <Input />
       </Form.Item>
-      <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email' }]}>
+
+      <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email',required: true  }]}>
         <Input />
       </Form.Item>
-      <Form.Item name={['user', 'age']} label="Age" rules={[{ type: 'number', min: 0, max: 99 }]}>
+
+      <Form.Item name={['user', 'age']} label="Graduation Year" rules={[{ type: 'number', min: 0, max: 2350, required: true  }]}>
         <InputNumber />
       </Form.Item>
-      <Form.Item name={['user', 'website']} label="Website">
+
+      <Form.Item name={['user', 'website']} label="Password" rules={[{ type: 'password',required: true  }]}>
         <Input />
+        Must be at least 6 characters
       </Form.Item>
-      <Form.Item name={['user', 'introduction']} label="Introduction">
-        <Input.TextArea />
-      </Form.Item>
+
       <Form.Item >
-        <Button type="primary" htmlType="submit">
-          Submit
+        Already have an account? <a href=""> Sign in </a> here
+        <Button type="primary" block htmlType="submit">
+          Get Started
         </Button>
       </Form.Item>
+
     </Form>
     </div>
   );
 };
 
-// class SignUpForm extends React.Component{
-//   render(){
-//     return (
-//       <div className="form container">
-//         <Form>
-//           <Form.Item>
-//           <Input label = "Username" value="Please enter a Username" />
-//           </Form.Item>
-//         </Form>
-//       </div>
-//   );
-//   }
-// }
 
 
 export default SignUpForm;
