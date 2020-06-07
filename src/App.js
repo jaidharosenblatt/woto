fc5249c9575b62715e59e55229071f33d6b73ff0
Kaden Rosenblatt <kaden@Kadens-Air.cable.rcn.com>
2020-06-07 16:52:05 -0500

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/signup/SignUp";
 import Help from "./pages/help/Help";
--- src/static/Images.js
+++ src/static/Images.js
@@ -9,3 +9,4 @@ export const ClockImage = require("./clock.svg");
 export const QueueImage = require("./queue.svg");
 export const ClockImageBlue = require("./clock-blue.svg");
 export const Logo = require("./Logo.svg");
+export const Circle = require("./circle.svg");
