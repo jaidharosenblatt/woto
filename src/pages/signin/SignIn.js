808d22d322ecbd4ed8a9fa4c51e1ce7e55695a94
Jaidha Rosenblatt <jrr59@duke.edu>
2020-06-10 08:33:59 -0500

    restyled and refactored signin page

--- src/pages/signin/SignIn.js
+++ src/pages/signin/SignIn.js
@@ -1,28 +1,69 @@
 import React from "react";
-import { Row, Col } from "antd";
-import "./SignIn.css"
-import SignInForm from "../signin/signinform/SignInForm";
-//import { Logo } from "../../static/Images";
-
+import { Form, Input, Button, Row } from "antd";
+import { Link } from "react-router-dom";
+import { Logo } from "../../static/Images";
 
 /**
- * @tommytilton User sign in
+ * @TommyTilton and @jaidharosenblatt
+ * Component used on Signin page
  */
+
+const validateMessages = {
+  required: "${label} is required!",
+  types: {
+    email: "${label} is not validate email!",
+    password: "${label} is not a validate password!",
+  },
+};
+
 const SignIn = () => {
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
-    <div className="SignInPage">
+    <div style={{ padding: "16px" }}>
       <Row align="center">
-        <Col xs={24} lg={24} align="center">
- 
-          <div className="signInForm">
-            <SignInForm style={{display: "inlineBlock"}} />
-          </div>
-        </Col>
+        <img src={Logo} style={{ marginBottom: "16px" }} alt="Woto Logo" />
+      </Row>
+      <Row align="center">
+        <Form
+          name="nest-messages"
+          layout="vertical"
+          style={{ width: "450px" }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          validateMessages={validateMessages}
+        >
+          <Form.Item
+            name={["user", "email"]}
+            label="Email"
+            rules={[{ type: "email", required: true }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name={["user", "password"]}
+            label="Password"
+            rules={[{ message: "Please input your password!", required: true }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" block htmlType="submit">
+              Sign In
+            </Button>
+          </Form.Item>
+          Don't have an account?
+          <Link to={"/signup"}> Sign up here </Link>
+        </Form>
       </Row>
     </div>
   );
 };
 
 export default SignIn;
-
-//         <img src={Logo} alt="Woto Logo" />
