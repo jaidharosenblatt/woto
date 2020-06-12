096de0bd9c376b046692f53b8e90f9f38c899271
Jaidha Rosenblatt <jrr59@duke.edu>
2020-06-12 13:56:50 -0500

    styled mobile menu icon

--- src/components/NavBar/NavBarMobile.js
+++ src/components/NavBar/NavBarMobile.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Layout, Space } from "antd";
 import { LogoWhite } from "../../static/Images";
+import { MenuOutlined } from "@ant-design/icons";
 import "./NavBar.css";
 
 const { Content, Sider } = Layout;
@@ -9,7 +10,9 @@ const { Content, Sider } = Layout;
 const NavBarMobile = () => {
   return (
     <Layout style={{ backgroundColor: "#40A9FF" }}>
-      <Sider width={50}>ham</Sider>
+      <Sider width={50} align="center">
+        <MenuOutlined className="MenuIcon" />
+      </Sider>
       <Content align="center">
         <Link to="/help">
           <img src={LogoWhite} alt="logo" className="Logo" />
