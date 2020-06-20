63299dcfca815bd61f5a3a7d21105c54e5c846b3
Jaidha Rosenblatt <jrr59@duke.edu>
2020-06-20 12:07:55 -0500

    fixed body on splash

--- src/pages/splash/SplashPage.js
+++ src/pages/splash/SplashPage.js
@@ -8,10 +8,10 @@ const SplashPage = () => {
   return (
     <div className="Splash">
       <Row align="center" style={{ backgroundColor: "#40A9FF" }}>
-        <div className="content">
+        <div className="header-wrapper">
           <Col align="left" xs={24} md={10}>
             <div className="headerText">
-              <h1>
+              <h1 className="header-title">
                 <b className="emphasize">Working together</b> to make office
                 hours more efficient for everyone
               </h1>
@@ -25,32 +25,45 @@ const SplashPage = () => {
           </Col>
         </div>
       </Row>
-
-      <Row align="middle">
-        <Col span={6} offset={3}>
-          <img className="tab" alt="tablet" src={Tablet} />
-        </Col>
-        <Col span={6} offset={6}>
-          Get behind-the-scenes data about how students learn
-        </Col>
-      </Row>
-      <Row align="middle">
-        <Col span={6} offset={6}>
-          Built for online
-        </Col>
-        <Col span={6} offset={-6}>
-          <img className="built" alt="video call" src={Built} />
-        </Col>
-      </Row>
-
-      <Row align="middle">
-        <Col span={6} offset={3}>
-          <img className="encourage" alt="collaboration" src={Encourage} />
-        </Col>
-        <Col span={6} offset={6}>
-          Encourage group learning
-        </Col>
-      </Row>
+      <div className="body-wrapper">
+        <Row align="middle">
+          <Col xs={0} md={11} align="left">
+            <img className="bodyImage" alt="tablet" src={Tablet} />
+          </Col>
+          <Col xs={24} md={13} align="left">
+            <h1>Get behind-the-scenes data about how students learn</h1>
+            <p>
+              Conveniently get insights about the performance of TAs, the types
+              of questions students are asking, and detailed feedback for
+              specific office hour sessions.
+            </p>
+          </Col>
+        </Row>
+        <Row align="middle">
+          <Col xs={24} md={13} align="left">
+            <h1>Built for online</h1>
+            <p>
+              Keep students in the know by informing them of their queue
+              position and expected wait time.
+            </p>
+          </Col>
+          <Col xs={0} md={11} align="right">
+            <img className="bodyImage" alt="online" src={Built} />
+          </Col>
+        </Row>
+        <Row align="middle">
+          <Col xs={0} md={11} align="left">
+            <img className="bodyImage" alt="encourage" src={Encourage} />
+          </Col>
+          <Col xs={24} md={13} align="left">
+            <h1>Encourage group learning</h1>
+            <p>
+              Allow students to <b> work together </b>while they wait for help
+              from teaching assistants.
+            </p>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
