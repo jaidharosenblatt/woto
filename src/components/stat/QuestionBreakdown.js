87375d744f2ea9a930e4e4db76bfe07e36950919
Yasser Elmzoudi <ye9@duke.edu>
2020-06-20 18:52:15 -0400

    Pie Chart Component for Question Breakdown

--- src/components/stat/QuestionBreakdown.js
+++ src/components/stat/QuestionBreakdown.js
@@ -1,83 +1,67 @@
-import { Menu, Dropdown, message } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
+import React from "react";
+import {
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+  Legend
+} from "recharts";
 
-const onClick = ({ key }) => {
-  message.info(`Click on item ${key}`);
-};
-
-const menu = (
-  <Menu onClick={onClick}>
-    <Menu.Item key="1">1st menu item</Menu.Item>
-    <Menu.Item key="2">2nd memu item</Menu.Item>
-    <Menu.Item key="3">3rd menu item</Menu.Item>
-  </Menu>
-);
-
-ReactDOM.render(
-  <Dropdown overlay={menu}>
-    <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-      Hover me, Click menu item <DownOutlined />
-    </a>
-  </Dropdown>,
-  mountNode,
-);
-
-
-
-
-
-
-
-
-
-import React, { PureComponent } from 'react';
-import PieChart from '@bit/recharts.recharts.pie-chart';
-import Pie from '@bit/recharts.recharts.pie';
-import Sector from '@bit/recharts.recharts.sector';
-import Cell from '@bit/recharts.recharts.cell';
 const data = [
-	{ name: 'Group A', value: 400 },
-	{ name: 'Group B', value: 300 },
-	{ name: 'Group C', value: 300 },
-	{ name: 'Group D', value: 200 },
+  { name: "Linked List", value: 400 },
+  { name: "Array", value: 300 },
+  { name: "Queue", value: 300 }
 ];
-
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-
+const COLORS = ["#40A9FF", "#91D5FF", "#595959"];
 const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = ({
-	cx, cy, midAngle, innerRadius, outerRadius, percent, index,
-}) => {
-	const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-	const x = cx + radius * Math.cos(-midAngle * RADIAN);
-	const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-	return (
-		<text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-			{`${(percent * 100).toFixed(0)}%`}
-		</text>
-	);
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
 };
-
-export default class Example extends PureComponent {
-	render() {
-		return (
-			<PieChart width={400} height={400}>
-				<Pie
-					data={data}
-					cx={200}
-					cy={200}
-					labelLine={false}
-					label={renderCustomizedLabel}
-					outerRadius={80}
-					fill="#8884d8"
-					dataKey="value"
-				>
-					{
-						data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-					}
-				</Pie>
-			</PieChart>
-		);
-	}
-}
+export default ({ name }) => (
+  <ResponsiveContainer width="100%" height={250}>
+    <PieChart height={250}>
+      <Pie
+        data={data}
+        cx="50%"
+        cy="50%"
+        outerRadius={100}
+        fill="#39a9"
+        dataKey="value"
+        label={renderCustomizedLabel}
+        labelLine={false}
+      >
+        {data.map((entry, index) => (
+          <Cell fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+      <Legend
+        verticalAlign="middle"
+        height={90}
+        layout="vertical"
+        align="right"
+        iconType="circle"
+      />
+    </PieChart>
+  </ResponsiveContainer>
+);
