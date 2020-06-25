5488daba8e6b8d57cdb3a67f03153111eeb8dbbf
Jaidha Rosenblatt <jrr59@duke.edu>
2020-06-24 22:34:18 -0500

    removed uncessary static images

--- src/static/Images.js
+++ src/static/Images.js
@@ -2,8 +2,7 @@
  * Component for accessing static images. When adding a new image,
  * add it to /static and export a new constant
  */
-export const thumbsDown = require("./thumbDownGray.svg");
-export const thumbsUp = require("./thumbUpBlue.svg");
+
 export const FrowmBlackOutline = require("./Frown.svg");
 export const HelpBlackOutline = require("./Vector.svg");
 export const SmileBlackImage = require("./smile.svg");
@@ -18,27 +17,12 @@ export const Logo = require("./Logo.svg");
 export const LogoWhite = require("./LogoWhite.svg");
 export const DefaultProfile =
   "https://3.bp.blogspot.com/-qDc5kIFIhb8/UoJEpGN9DmI/AAAAAAABl1s/BfP6FcBY1R8/s1600/BlueHead.jpg";
-export const Kaden = require("./Kaden.jpg");
 export const Hourglass = require("./hourglass.svg");
 export const ZoomVideoImage = require("./zoomvideoicon.svg");
 export const PersonOutline = require("./personoutline.svg");
-export const AssignmentIcon = require("./assignment.svg");
-export const PageIcon = require("./pageicon.svg");
-export const ListIcon = require("./listicon.svg");
-export const HelpIcon = require("./helpicon.svg");
 export const Bell = require("./bell.svg");
-export const Spiderman = require("./spiderman.svg");
-export const ThumbsUp = require("./thumbsup.svg");
-export const ThumbsDown = require("./thumbsdown.svg");
-export const Plus = require("./plus.svg");
 export const Video = require("./video.svg");
 export const Encourage = require("./encourage.svg");
 export const Built = require("./built.svg");
 export const Tablet = require("./tablet.svg");
 export const Homies = require("./homies.svg");
-export const BlueQuestionMark = require("./bluequestionmark.svg");
-export const GrayQuestionMark = require("./grayquestionmark.svg");
-export const BluePencil = require("./bluepencil.svg");
-export const GrayClipboard = require("./grayclipboard.svg");
-export const GrayPage = require("./graypage.svg");
-export const GrayLines = require("./graylines.svg");
