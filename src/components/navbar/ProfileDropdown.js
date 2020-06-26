75993079489f5f69f2c6fe2d28ecc309f68625b5
Jaidha Rosenblatt <jrr59@duke.edu>
2020-06-26 15:20:41 -0500

    added logout

--- src/components/navbar/ProfileDropdown.js
+++ src/components/navbar/ProfileDropdown.js
@@ -2,6 +2,7 @@ import React from "react";
 import { SettingOutlined, LogoutOutlined } from "@ant-design/icons";
 import { Menu } from "antd";
 import { Link } from "react-router-dom";
+import { logOut } from "../../api/API";
 
 /**
  * @kadenrosenblatt Dropdown to display when a user clicks on their avatar in navbar
@@ -14,7 +15,7 @@ const ProfileDropdown = () => {
           <SettingOutlined /> Account Settings
         </Link>
       </Menu.Item>
-      <Menu.Item>
+      <Menu.Item onClick={logOut}>
         <Link to="/signin">
           <LogoutOutlined /> Log out
         </Link>
