5d74e4246ccb55d9fec2f8a6f58005f48eca5fc0
Jaidha Rosenblatt <jrr59@duke.edu>
2020-07-16 12:02:29 -0700

    cleaned up splash and fixed footer

--- src/pages/about/DisplayCards.js
+++ src/pages/about/DisplayCards.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import {Card, Row} from 'antd';
+import React from "react";
+import { Card, Col, Row } from "antd";
 const { Meta } = Card;
 
-const DisplayCards = ({people}) =>{
-    var renderedCards = [];
-    
-    people.forEach((person) => {
-        renderedCards.push(
-        <Card style={{width:"200px"}} cover={<img alt="example" src= {person.image}/>} >
-         <Meta title ={person.name} description={person.title} />
-        </Card>);
-      });
-   
-     return (<Row gutter={[20,20]}>{renderedCards} </Row>);
+const DisplayCards = ({ people }) => {
+  var renderedCards = [];
+
+  people.forEach((person) => {
+    renderedCards.push(
+      <Card cover={<img alt="example" src={person.image} />}>
+        <Meta title={person.name} description={person.title} />
+      </Card>
+    );
+  });
+
+  return <Row>{renderedCards} </Row>;
 };
-    
+
 export default DisplayCards;
