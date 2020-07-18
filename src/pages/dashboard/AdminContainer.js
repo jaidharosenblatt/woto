c4aeab55095c4fdd7e1625496b09ebda76c2c1c5
Jaidha Rosenblatt <jrr59@duke.edu>
2020-07-18 16:33:15 -0700

    cleaned up admin container

--- src/pages/dashboard/AdminContainer.js
+++ src/pages/dashboard/AdminContainer.js
@@ -31,28 +31,10 @@ const AdminContainer = (props) => {
     });
   });
 
-  // const styles = {
-  //   adminNavbar: {
-  //     zIndex: 3,
-  //     position: "fixed",
-  //     height: "100vh",
-  //     backgroundColor: "rgb(247, 247, 247)",
-  //     padding: "0px",
-  //   },
-
-  //   layoutStyles: {
-  //     backgroundColor: "rgb(247, 247, 247)",
-  //   },
-  //   contentStyles: {
-  //     backgroundColor: "rgb(247, 247, 247)",
-  //     margin: "24px 12px 0",
-  //   },
-  // };
-
   return (
-    <Layout>
+    <Layout className="admin">
       <Layout.Sider
-        width={220}
+        width="220"
         breakpoint="lg"
         collapsedWidth="0"
         onCollapse={(collapsed) => {
@@ -61,28 +43,23 @@ const AdminContainer = (props) => {
       >
         <AdminNavBar courses={courses} />
       </Layout.Sider>
-      <Layout.Content>
-        <Layout>
-          <Layout.Header>
-            <AvatarDropdown showName />
-          </Layout.Header>
-          {/* <div
-            className="admin-navbar-wrapper"
-            style={{
-              width: screenSizeSmall ? "100%" : "calc(100vw - 220px)",
-            }}
-          >
-            
-          </div> */}
-          <div className="AdminBody" style={{ padding: 24 }}>
-            <Switch>
-              {pages}
-              {props.routes}
-              {props.redirects}
-            </Switch>
-          </div>
-        </Layout>
-      </Layout.Content>
+      <Layout>
+        <div
+          className="admin-navbar-wrapper"
+          style={{
+            width: screenSizeSmall ? "100%" : "calc(100vw - 220px)",
+          }}
+        >
+          <AvatarDropdown showName />
+        </div>
+        <div className="AdminBody" style={{ padding: 24 }}>
+          <Switch>
+            {pages}
+            {props.routes}
+            {props.redirects}
+          </Switch>
+        </div>
+      </Layout>
     </Layout>
   );
 };
