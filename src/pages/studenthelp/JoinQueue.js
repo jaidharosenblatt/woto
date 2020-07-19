b1101dc03b7a4b16657ac2320802ef47ef3001b0
Jaidha Rosenblatt <jrr59@duke.edu>
2020-07-19 16:44:47 -0700

    restyled join session page

--- src/pages/studenthelp/JoinQueue.js
+++ src/pages/studenthelp/JoinQueue.js
@@ -1,39 +1,53 @@
 import React from "react";
-import { Row, Col, Space, Button } from "antd";
+import { Row, Col, Space, Button, Card } from "antd";
 import LocationTimeTag from "../../components/header/LocationTimeTag";
-import { ProblemImage } from "../../static/Images";
+import { PresentationImage, Encourage } from "../../static/Images";
 import "./Help.css";
 
 const JoinQueue = ({ setStage, course, queueSize = 1 }) => {
   return (
-    <Row align="middle">
-      <Col xs={24} md={12} align="middle">
-        <img className="hero" src={ProblemImage} alt="waitng" />
-      </Col>
-      <Col xs={24} md={12} align="left">
-        <Space direction="vertical" className="session-details">
-          <div>
-            <h1>{`${course.code} Office Hours`}</h1>
-            <LocationTimeTag location="Virtual" time="Now until 4pm" />
-          </div>
-          <p>
-            Reserve your spot now before submitting a question. You will still
-            have the option to collaborate once you submit your question
-          </p>
-          <Button
-            type="primary"
-            onClick={() => setStage("submit")}
-          >{`Join the TA Queue As #${queueSize}`}</Button>
-          <p>
-            Or, if you don't need help from a teaching assistant and are just
-            looking to collaborate with peers
-          </p>
-          <Button
-            onClick={() => setStage("collab")}
-          >{`Join ${course.code}'s Woto Room`}</Button>
-        </Space>
-      </Col>
-    </Row>
+    <div className="join-queue-wrapper">
+      <Row className="join-queue" align="middle">
+        <Col xs={24} md={12}>
+          <Card>
+            <img className="hero" src={PresentationImage} alt="waitng" />
+            <div className="session-details">
+              <Space direction="vertical">
+                <h1>Office Hours</h1>
+                {/* <LocationTimeTag location="Virtual" time="Now until 4pm" /> */}
+                <p>
+                  Reserve your spot to work with a TA now and join the Woto Room
+                  later question
+                </p>
+                <Button
+                  type="primary"
+                  block
+                  onClick={() => setStage("submit")}
+                >{`Join the Queue As #${queueSize}`}</Button>
+              </Space>
+            </div>
+          </Card>
+        </Col>
+        <Col xs={24} md={12}>
+          <Row style={{ height: "100%" }} align="bottom">
+            <Card>
+              <img className="hero" alt="working together" src={Encourage} />
+              <Space direction="vertical" className="session-details">
+                <h1>Woto Room</h1>
+                <p>
+                  If you don't need help from a teaching assistant and are just
+                  looking to work together with peers
+                </p>
+                <Button
+                  block
+                  onClick={() => setStage("collab")}
+                >{`Join ${course.code}'s Woto Room`}</Button>
+              </Space>
+            </Card>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
