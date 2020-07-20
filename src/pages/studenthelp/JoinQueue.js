a0289076ad889d41077d1852c7809415d86e452c
Jaidha Rosenblatt <jrr59@duke.edu>
2020-07-19 18:26:28 -0700

    improved join session responsiveness

--- src/pages/studenthelp/JoinQueue.js
+++ src/pages/studenthelp/JoinQueue.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Row, Col, Space, Button, Card } from "antd";
-import LocationTimeTag from "../../components/header/LocationTimeTag";
 import { PresentationImage, Encourage } from "../../static/Images";
 import "./Help.css";
 
@@ -8,13 +7,12 @@ const JoinQueue = ({ setStage, course, queueSize = 1 }) => {
   return (
     <div className="join-queue-wrapper">
       <Row className="join-queue" align="middle">
-        <Col xs={24} md={12}>
+        <Col xs={24} sm={12}>
           <Card>
-            <img className="hero" src={PresentationImage} alt="waitng" />
-            <div className="session-details">
+            <div className="card-details">
+              <img className="hero" src={PresentationImage} alt="waitng" />
               <Space direction="vertical">
-                <h1>Office Hours</h1>
-                {/* <LocationTimeTag location="Virtual" time="Now until 4pm" /> */}
+                <h1>Office Hours Until 4pm</h1>
                 <p>
                   Reserve your spot to work with a TA now and join the Woto Room
                   later question
@@ -28,23 +26,24 @@ const JoinQueue = ({ setStage, course, queueSize = 1 }) => {
             </div>
           </Card>
         </Col>
-        <Col xs={24} md={12}>
-          <Row style={{ height: "100%" }} align="bottom">
-            <Card>
+        <Col xs={24} sm={12}>
+          <Card>
+            <div className="card-details">
               <img className="hero" alt="working together" src={Encourage} />
               <Space direction="vertical" className="session-details">
                 <h1>Woto Room</h1>
                 <p>
-                  If you don't need help from a teaching assistant and are just
-                  looking to work together with peers
+                  If you don't need help from a teaching assistant and want to
+                  go straight to working together with peers
                 </p>
                 <Button
+                  type="primary"
                   block
                   onClick={() => setStage("collab")}
                 >{`Join ${course.code}'s Woto Room`}</Button>
               </Space>
-            </Card>
-          </Row>
+            </div>
+          </Card>
         </Col>
       </Row>
     </div>
