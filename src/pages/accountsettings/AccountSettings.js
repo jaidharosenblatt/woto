7a7408885f7d41c94398786e4f7c07ae6be87391
Jaidha Rosenblatt <jrr59@duke.edu>
2020-07-29 12:59:53 -0700

    made navbar centered component and updated verify email

--- src/pages/accountsettings/AccountSettings.js
+++ src/pages/accountsettings/AccountSettings.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { Card, Row, Space } from "antd";
 import ProfileForm from "./ProfileForm";
--- src/pages/accountsettings/EditCourses.js
+++ src/pages/accountsettings/EditCourses.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import { Card, List, Button } from "antd";
 import { Link } from "react-router-dom";
 import "./AccountSettings.css";
