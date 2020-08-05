6c8c833d87f41396e71b9f24d035456f81f5c550
Jaidha Rosenblatt <jrr59@duke.edu>
2020-08-05 08:33:44 -0700

    temp fix for broken helper table

--- src/pages/tahelp/TAContentTabs.js
+++ src/pages/tahelp/TAContentTabs.js
@@ -33,11 +33,13 @@ const TAContentTabs = (props) => {
           key="queue"
         >
           <Space direction="vertical" style={{ width: "100%" }}>
-            <SessionTable
+            <CollabTable taPage session={props.session} course={props.course} />
+
+            {/* <SessionTable
               taPage
               session={props.session}
               course={props.course}
-            />
+            /> */}
           </Space>
         </Tabs.TabPane>
         <Tabs.TabPane
