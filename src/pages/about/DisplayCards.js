68be621e8d56f3823e35ad40abae5090f95c36a2
Scott Pledger <stpledger@gmail.com>
2020-09-30 10:53:06 -0400

    Updated styling for About page and allowed for short descriptions of team members on hover.

--- src/pages/about/DisplayCards.js
+++ src/pages/about/DisplayCards.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Card, Row } from "antd";
 const { Meta } = Card;
 
 const DisplayCards = ({ people }) => {
   var renderedCards = [];
 
+  const description = (description) =>  {
+    return description ? (
+      <div className="describe">
+          <p>{description}</p>
+      </div>
+    ) : (
+      null
+    );
+  };
+
   people.forEach((person, key) => {
     renderedCards.push(
-      <Card key={key} cover={<img alt="example" src={person.image} />}>
-        <Meta title={person.name} description={person.title} />
-      </Card>
+      <div key={key} className="card-wrapper">
+        <Card bordered={false} className="person" cover={<img alt="example" src={person.image} />}>
+          <Meta title={person.name} description={person.title} />
+        </Card>
+        {description(person.description)}
+      </div>
     );
   });
 
-  return <Row>{renderedCards} </Row>;
+  return <Row justify="center">{renderedCards} </Row>;
 };
 
 export default DisplayCards;
