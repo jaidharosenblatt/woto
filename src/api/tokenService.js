a0245ec2ea4b0ac331df809e3707b433aae68ba1
Jaidha Rosenblatt <jrr59@duke.edu>
2020-12-10 23:03:16 -0800

    switched from storing JWT token in localStorage to cookies

--- src/api/tokenService.js
+++ src/api/tokenService.js
@@ -1,17 +1,19 @@
+import Cookies from "js-cookie";
+
 // Set the JWT as "token" in local storage
 export function setToken(JWT) {
   // Set an object called token with value and expiry properties
-  localStorage.setItem("token", JSON.stringify(JWT));
+  Cookies.set("token", JSON.stringify(JWT));
 }
 
 // Clear the current JWT from local storage
 export function clearToken() {
-  localStorage.removeItem("token");
+  Cookies.remove("token");
 }
 
 // Get the current JWT from local storage
 export function getToken() {
-  const token = localStorage.getItem("token");
+  const token = Cookies.get("token");
   if (token && token !== "undefined") {
     return JSON.parse(token);
   }
@@ -21,12 +23,12 @@ export function getToken() {
 
 // set the current user type "instructor" or "student"
 export function setUserType(userType) {
-  window.localStorage.setItem("userType", JSON.stringify(userType));
+  Cookies.set("userType", JSON.stringify(userType));
 }
 
 // Get the current user from local storage
 export function getUserType() {
-  const userType = localStorage.getItem("userType");
+  const userType = Cookies.get("userType");
   if (userType) {
     return JSON.parse(userType);
   }
@@ -35,5 +37,5 @@ export function getUserType() {
 
 // Clear the current user from local storage
 export function clearUserType() {
-  localStorage.removeItem("userType");
+  Cookies.remove("userType");
 }
