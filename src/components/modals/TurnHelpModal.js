84088221865d30fe1b6a95cee81c00890a1ccad1
Matthew Sclar <sclarmatthew@gmail.com>
2020-12-19 14:01:09 -0500

    Added new WOTO sound plays on set interval

--- src/components/modals/TurnHelpModal.js
+++ src/components/modals/TurnHelpModal.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Space, Col } from "antd";
 import ProfileBlock from "./tools/ProfileBlock";
 import { BellIcon } from "./tools/Icons";
 import { connect } from "react-redux";
 import selectors from "../../redux/selectors";
+
+import soundfile from "../../static/audio/NotificationAudio.mp3";
 
 import { joinTAVideoLink } from "../../redux/courses/actions/student";
 import { Redirect } from "react-router-dom";
@@ -17,6 +19,37 @@ import { Redirect } from "react-router-dom";
  */
 const TurnHelpModal = (props) => {
   const user = { name: props.assistant?.name, role: props.assistant?.role };
+
+  var PageTitleNotification = {
+    Vars: {
+      OriginalTitle: document.title,
+      Interval: null,
+    },
+    On: function (notification, intervalSpeed) {
+      var _this = this;
+      _this.Vars.Interval = setInterval(
+        function () {
+          document.title =
+            _this.Vars.OriginalTitle === document.title
+              ? notification
+              : _this.Vars.OriginalTitle;
+        },
+        intervalSpeed ? intervalSpeed : 1000
+      );
+    },
+    Off: function () {
+      clearInterval(this.Vars.Interval);
+      document.title = this.Vars.OriginalTitle;
+    },
+  };
+
+  useEffect(() => {
+    const audioAlert = document.getElementsByClassName("audio-alert")[0];
+    audioAlert.play();
+    PageTitleNotification.On("Help Readyy", 0);
+    setTimeout(PageTitleNotification.Off(), 10000);
+  }, [PageTitleNotification]);
+
   return (
     <Col align="middle">
       <Redirect to={`/courses/${props.courseID}/session`} />
@@ -36,6 +69,11 @@ const TurnHelpModal = (props) => {
           Join Video Room
         </Button>
       </Space>
+      <div>
+        <audio className="audio-alert">
+          <source src={soundfile}></source>
+        </audio>
+      </div>
     </Col>
   );
 };
