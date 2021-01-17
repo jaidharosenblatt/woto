84dc624e04b212f0270cfd12d7fc69ea42623045
Jaidha Rosenblatt <jrr59@duke.edu>
2021-01-17 09:45:40 -0800

    improved selectwithadd by using trie package

--- src/components/form/SelectWithAdd.js
+++ src/components/form/SelectWithAdd.js
@@ -1,12 +1,29 @@
 import React, { useState } from "react";
 import { Select } from "antd";
+import TrieSearch from "trie-search";
 
+/**
+ * Render a select field that allows searching of items and additional options
+ * It's a shame that Ant.design doesn't support this already...
+ * @param {Array} options list of options to render
+ * @param {Object} propsWithoutOptions any other props passed in
+ */
 export default function SelectWithAdd(props) {
   const { options, ...propsWithoutOptions } = props;
   const [optionsWithSearch, setOptionsWithSearch] = useState(options);
 
+  // use a trie package to get quick checks for prefixes
+  const optionsTree = new TrieSearch("value");
+  optionsTree.addAll(
+    options.map((option) => {
+      return { value: option };
+    })
+  );
+
+  // add the current search value to options if it doesn't already exist
   const handleSearch = (search) => {
-    if (search?.length > 0) {
+    const existsInOptions = optionsTree.get(search).length > 0;
+    if (!existsInOptions && search?.length > 0) {
       setOptionsWithSearch(options.concat(search));
     }
   };
