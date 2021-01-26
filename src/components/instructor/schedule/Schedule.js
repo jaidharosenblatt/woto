0ec64bf27219d2efe7f61d547d160334757f78bc
Jaidha Rosenblatt <jrr59@duke.edu>
2021-01-26 14:41:22 -0500

    added assistants to schedule from redux

--- src/components/instructor/schedule/Schedule.js
+++ src/components/instructor/schedule/Schedule.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchRoster } from "../../../redux/courses/actions/roster";
+import selectors from "../../../redux/selectors";
 import ScheduleTimeSelector from "./ScheduleTimeSelector";
 
-export default function Schedule() {
+const Schedule = (props) => {
+  const { assistants } = props;
+  const _fetchRoster = props.fetchRoster;
+
+  useEffect(() => {
+    async function fetch() {
+      await _fetchRoster();
+    }
+    // only fetch if data is null
+    if (!assistants) {
+      fetch();
+    }
+  }, [_fetchRoster, assistants]);
+
   return (
     <div>
+      <h2>Assistants</h2>
+      {assistants?.map((assistant, i) => (
+        <p key={i}>{assistant.name}</p>
+      ))}
       Jasmine - put code here <ScheduleTimeSelector />
     </div>
   );
-}
+};
+
+const mapStateToProps = (state) => ({
+  assistants: selectors.getTARoster(state),
+});
+export default connect(mapStateToProps, { fetchRoster })(Schedule);
