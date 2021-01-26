d9ebf651962fd595d4a7ca9a40a6c1aab5c16375
Jaidha Rosenblatt <jrr59@duke.edu>
2021-01-26 13:47:25 -0500

    modify timeselector to round up

--- src/components/ta/openjoin/TimeSelector.js
+++ src/components/ta/openjoin/TimeSelector.js
@@ -54,7 +54,7 @@ const TimeSelector = ({ startTime, endTime }) => {
         <Col>
           <Form.Item
             style={{ width: "150px" }}
-            initialValue={options[0].key}
+            initialValue={options[1].key}
             name="startTime"
           >
             <Select style={{ width: "100%" }} showSearch>
