04f321ae3e79a956ab7fe403a4f251666941411e
joshu <joshuapetitma@yahoo.com>
2021-02-24 16:18:14 -0500

    Use counter for wait time interval

--- src/components/ta/HelpStudents.js
+++ src/components/ta/HelpStudents.js
@@ -18,6 +18,7 @@ const HelpStudents = (props) => {
   const helped = props.stats.helped;
   const [notHelpedData, setNotHelpedData] = useState([]);
   const [helpedData, setHelpedData] = useState([]);
+  const [waitTime, setWaitTime] = useState(0);
 
   const [showAll, setShowAll] = useState(false);
 
@@ -28,22 +29,25 @@ const HelpStudents = (props) => {
     const audio = new Audio(soundfile);
 
     const name = notHelped && notHelped[0]?.student?.name;
-    const interval = setInterval(() => {
-      if (!activeQuestion && name) {
-        audio.play();
-        addNotification({
-          title: "Students are in the Queue",
-          message: `${name} is waiting to be helped`,
-          native: true, // when using native, your OS will handle theming.
-        });
-      }
-    }, 10000);
+
+    if (!activeQuestion && name && waitTime < 3) {
+      setWaitTime(waitTime + 1);
+    } else if (activeQuestion) {
+      setWaitTime(0);
+    }
     
+    if (!activeQuestion && name && waitTime > 2) {
+      audio.play();
+      addNotification({
+        title: "A Student Joined the Queue",
+        message: `${name} is waiting to be helped`,
+        native: true, // when using native, your OS will handle theming.
+      });
+    }
+
     setHelpedData(convertHelpData(helped));
     setNotHelpedData(convertHelpData(notHelped));
-    
-    return () => clearInterval(interval);
-  }, [questions, activeQuestion, helped]);
+  }, [questions, activeQuestion, helped, waitTime]);
 
   const endInteraction = async () => {
     props.finishHelpingStudent();
