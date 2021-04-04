ef0bdc5ba0e11b958e0a6029ef200cfb9db7a392
joshu <joshuapetitma@yahoo.com>
2021-04-04 07:54:46 -0400

    Fix import for zoomOauthUrl

--- src/components/user/oauth/ZoomAuthButton.js
+++ src/components/user/oauth/ZoomAuthButton.js
@@ -1,6 +1,6 @@
 import { Button, Space } from "antd";
 import React from "react";
-import useZoomOauthUrl from "../../../hooks/useOauthUrl";
+import { useZoomOauthUrl } from "../../../hooks/useOauthUrl";
 import { ZoomLogo } from "../../../static/LoadedImages";
 
 /**
